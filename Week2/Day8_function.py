24e2779806d5210e4be5c65e6bb09ff36fa7436d
Peter Adewale Ayoola <122322908+Emperor1p@users.noreply.github.com>
2023-06-26 00:16:49 +0100

    Day 8 function

---
+++ Week2/Day8_function.py
@@ -0,0 +1,21 @@
+
+#function
+name = "Peter"
+response = input("Enter your response about the weather: ")
+def greet(name):
+    print("Hello")
+    print(f"How do you do {name}? ")
+    print(f"Isn't the weather nice today {name}? {response}")
+    return 
+greet(name)
+
+
+#function
+
+def greet(name, response):
+    print("Hello")
+    print(f"How do you do {name}? ")
+    print(f"Isn't the weather nice today {name}? {response}")
+    return 
+greet(name, response)
+
