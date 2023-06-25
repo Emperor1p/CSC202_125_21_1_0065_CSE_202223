abacdc97232e42cce218bf819c2e198d527912ba
Peter Adewale Ayoola <122322908+Emperor1p@users.noreply.github.com>
2023-06-25 23:45:49 +0100

    Day1

---
+++ Day1.py
@@ -0,0 +1,97 @@
+#DAY1 Printing start, for output
+print('Hello World!')
+print('Day 1- python print function')
+print('The function is declare like this:')
+print('Hello world!\nHello world!')
+
+
+#strings
+name = 'Peter'
+type(name)
+
+#string concatenation
+print('Hello' " " ' Angela')
+print('Hello' + " " + 'Angela')
+
+#input function will get user input in console
+print('What is your name? ')
+input('A prompt for user')
+input('What is your name? ')
+
+#input exercise
+input('What is your name? ')
+
+#printing the len of the input
+print(len(input(' What is your name ? ')))
+
+#python variable naming
+name = 'JACK'
+print(name)
+name = input('What is your name? ')
+length = len(name)
+print('Your name is ' + name)
+print(length)
+
+#variable name generator
+#1. Create a greeting for your program
+#2. Ask the user for the city that they grew up in
+#3. Ask the user for the name of a pet
+#4. Combine the name their city and pet and show them their band name
+#5. Make sure the input cursor shows on a new line
+
+print('Hello User, Welcome to the band name generator.')
+city = input('Enter the name of the city you grew up in: ')
+print(city)
+pet = input('What is the name of a pet?')
+print(pet)
+print('Your band name could be ' + city + " " + pet)
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+#DAY 1.2
+print('string concatenation is done with the " + " sign ')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+print('Hello' + " " + input(' what is your name? '))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
